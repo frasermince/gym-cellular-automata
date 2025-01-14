7329b0a9aec7fc9d7382529ed7b59b08db611aa5
Fraser Mince <frasermince@gmail.com>
2025-01-13 23:13:40 -0600

    Move to average of five.

--- gym_cellular_automata/agents/jax_ppo.py
+++ gym_cellular_automata/agents/jax_ppo.py
@@ -353,8 +353,8 @@ def run_rollout_loop(
         episode_lengths=jnp.zeros(num_envs, dtype=jnp.int32),
         returned_episode_returns=jnp.zeros(num_envs, dtype=jnp.float32),
         returned_episode_lengths=jnp.zeros(num_envs, dtype=jnp.int32),
-        recent_returns=jnp.zeros(50, dtype=jnp.float32),
-        recent_lengths=jnp.zeros(50, dtype=jnp.int32),
+        recent_returns=jnp.zeros(5, dtype=jnp.float32),
+        recent_lengths=jnp.zeros(5, dtype=jnp.int32),
         recent_idx=jnp.array(0, dtype=jnp.int32),
     )
     # handle, recv, send, step_env = envs.xla()
@@ -392,7 +392,7 @@ def run_rollout_loop(
                     stats.recent_lengths.at[new_idx].set(lengths[env_idx]),
                     stats.recent_lengths,
                 )
-                new_idx = (new_idx + mask[env_idx].astype(jnp.int32)) % 50
+                new_idx = (new_idx + mask[env_idx].astype(jnp.int32)) % 5
 
                 return (
                     (
@@ -413,9 +413,7 @@ def run_rollout_loop(
                 body_fun, (stats, returns, lengths, mask), jnp.arange(mask.shape[0])
             )
 
-            return final_stats.replace(
-                recent_idx=(stats.recent_idx + num_finished) % 50
-            )
+            return final_stats.replace(recent_idx=(stats.recent_idx + num_finished) % 5)
 
         finished_mask = next_info["terminated"] + next_info["TimeLimit.truncated"]
         current_episode_stats = update_recent_stats(
@@ -1080,8 +1078,8 @@ def run_rollout_loop(
             "avg_episode_length": "0.0",
             # "avg_returned_episode_length": "0.0",
             "avg_return_per_timestep": "0.0",
-            "recent_50_return": "0.0",
-            "recent_50_length": "0.0",
+            "recent_5_return": "0.0",
+            "recent_5_length": "0.0",
             "value_loss": "0.0",
             "policy_loss": "0.0",
             "entropy_loss": "0.0",
@@ -1235,8 +1233,8 @@ def run_rollout_loop(
                     "games_finished": int(jax.device_get(total_finished)),
                     # "avg_returned_episode_length": f"{avg_returned_episode_length:.2f}",
                     "avg_return_per_timestep": f"{recent_avg_return/max(recent_avg_length, 1e-8):.4f}",
-                    "recent_50_return": f"{recent_avg_return:.2f}",
-                    "recent_50_length": f"{recent_avg_length:.2f}",
+                    "recent_5_return": f"{recent_avg_return:.2f}",
+                    "recent_5_length": f"{recent_avg_length:.2f}",
                     "value_loss": f"{v_loss.item():.4f}",
                     "policy_loss": f"{pg_loss.item():.4f}",
                     "entropy_loss": f"{entropy_loss.item():.4f}",
